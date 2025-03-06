d8dcaae1dd72223a5029daf37c2b3bfd8338c8d1
AlVFino <aaakuuu36@gmail.com>
2025-03-06 10:08:58 +0700

    update halaman AboutMe

--- src/HalamanRouterReact/AboutMe.jsx
+++ src/HalamanRouterReact/AboutMe.jsx
@@ -8,26 +8,7 @@ function AboutMe() {
 
     <section className="py-10 bg-gray-100 sm:py-16 lg:py-24">
       <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-        <div className="grid items-center grid-cols-1 gap-y-8 lg:grid-cols-2 gap-x-16 xl:gap-x-24">
-          <div className="relative mb-12">
-            <img
-              className="w-full rounded-md"
-              src="https://images.unsplash.com/photo-1531482615713-2afd69097998?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="Team Work"
-            />
-            <div className=" absolute w-full max-w-xs px-4 -translate-x-1/2 sm:px-0 sm:max-w-sm left-1/2 -bottom-12">
-              <div className="overflow-hidden bg-white rounded">
-                <div className="px-10 py-6">
-                  <div className="flex items-center">
-                    <p className="flex-shrink-0 text-3xl font-bold text-blue-600 sm:text-4xl">100%</p>
-                    <p className="pl-6 text-sm font-medium text-black sm:text-lg">
-                      FREE <br /> for Teacher And Study
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="mt-5 pt-10">
 
           <div>
             <h2 className=" text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl lg:leading-tight">
